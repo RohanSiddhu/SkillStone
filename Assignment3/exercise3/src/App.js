aa0136fcfbb09d0c4ea62246d745aff734237397
Rohan Siddhu <rohansiddhu21@gmail.com>
2023-07-04 11:59:47 +0530

    Completed Assignment3/exercise3

--- Assignment3/exercise3/src/App.js
+++ Assignment3/exercise3/src/App.js
@@ -1,8 +1,14 @@
 import styled from "styled-components";
 
+const Heading = styled.h1`
+  color: #ffd969;
+  text-align: center;
+`
+
 function App() {
   return (
     <div className="App">
+      <Heading>Hello React</Heading>
     </div>
   );
 }
